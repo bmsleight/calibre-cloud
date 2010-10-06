ddf1450c572be837ea4b260cde5b73d967240d70
bms@barwap.com <bms@barwap.com@d3ff22da-d418-0fff-b7c5-f4dd303b90d2>
2010-10-06 21:47:16 +0000

    Add wsgi handler and apache config

---
+++ calibreupload/wsgi_handler.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'calibreupload.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+
